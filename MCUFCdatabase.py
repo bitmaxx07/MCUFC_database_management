c8e850fad6b86d78b8079600c3e2759e30abc514
bitmaxx07 <84411099+bitmaxx07@users.noreply.github.com>
2022-12-20 21:34:02 +0100

    updated

--- MCUFCdatabase.py
+++ MCUFCdatabase.py
@@ -1,1 +1,8 @@
+import jwt
 
+key = "secret"
+encoded = jwt.encode({"chengqi": ""}, key, algorithm="HS256")
+print(encoded.replace(".", "-"))
+print(encoded.replace(".", "-")[0: 75].lower())
+
+print(jwt.decode(encoded, key, algorithms="HS256"))
