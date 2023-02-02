f242dc09a4a1bd46857bb2288190e49498c8c2ad
bitmaxx07 <84411099+bitmaxx07@users.noreply.github.com>
2023-02-02 21:37:32 +0100

    updated

--- MCUFCdatabase.py
+++ MCUFCdatabase.py
@@ -12,22 +12,27 @@ key = "secret"
 
 id_dict = {}
 
-wb = openpyxl.load_workbook("namelist.xlsx")
-ws = wb.active
+wb = openpyxl.load_workbook("会员卡.xlsx")
+ws = wb["Fussball Mitgliederlist"]
 
-col_name = 1
-col_id = 2
-col_subname = 4
-col_website = 3
+col_lastname = 1
+col_firstname = 2
+col_birthday = 3
+col_id = 4
+col_subname = 6
+col_website = 5
 
-for row in range(2, ws.max_row + 1):
+for row in range(1, ws.max_row + 1):
     # fill in excel sheet and generate id mapping
-    encoded = jwt.encode({ws.cell(row, col_name).value: ""}, key, algorithm="HS256")
+    code_string = ws.cell(row, col_lastname).value + "_" + \
+                  ws.cell(row, col_firstname).value + "_" + \
+                  str(ws.cell(row, col_birthday).value)
+    encoded = jwt.encode({code_string: ""}, key, algorithm="HS256")
     ws.cell(row, col_id).value = encoded
     ws.cell(row, col_subname).value = encoded.replace(".", "-")[0: 75].lower()
-    id_dict.update({ws.cell(row, col_name).value: encoded.replace(".", "-")[0: 75].lower()})
+    id_dict.update({code_string: encoded.replace(".", "-")[0: 75].lower()})
     ws.cell(row, col_website).value = "https://www.mcu-fc.com/" + encoded.replace(".", "-")[0: 75].lower()
-    print("name: " + ws.cell(row, col_name).value)
+    print("name/birthday: " + code_string)
     print("id: " + ws.cell(row, col_id).value)
     print("subname: " + ws.cell(row, col_subname).value)
     print("website: " + ws.cell(row, col_website).value)
@@ -40,12 +45,12 @@ for row in range(2, ws.max_row + 1):
     qr.make(fit=True)
 
     img = qr.make_image(fill="black", back_color="white")
-    img.save("qrcodes/" + ws.cell(row, col_name).value + ".png")
-    print("saved QR code for " + ws.cell(row, col_name).value)
+    img.save("qrcodes/" + code_string + ".png")
+    print("saved QR code for " + code_string)
     print("------------------------")
 
 
-wb.save("namelist.xlsx")
+wb.save("会员卡.xlsx")
 print("-----------FINISHED----------")
 print("TOTAL MAPPING:")
 for key in id_dict.keys():
